987c6daa4729d87677607ad76ec2a9dd8860a711
Lior Banai <liorbanai@gmail.com>
2019-11-16 13:18:43 +0200

    Analogy.Interface project

---
+++ Analogy.Interfaces/ILogMessageCreatedHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Analogy.Interfaces
+{
+    public interface ILogMessageCreatedHandler
+    {
+        bool ForceNoFileCaching { get; set; }
+        bool DoNotAddToRecentHistory { get; set; }
+        void AppendMessage(AnalogyLogMessage message, string dataSource);
+        void AppendMessages(List<AnalogyLogMessage> messages, string dataSource);
+
+    }
+}

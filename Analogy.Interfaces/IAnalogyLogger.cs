fda54d5e2d8e865db271832b6725de680ef35623
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-01-31 12:04:39 +0200

    Add caller member attributes to ILogger fix #8

--- Analogy.Interfaces/IAnalogyLogger.cs
+++ Analogy.Interfaces/IAnalogyLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Analogy.Interfaces
@@ -9,15 +10,15 @@ namespace Analogy.Interfaces
     /// </summary>
     public interface IAnalogyLogger
     {
-        void LogEvent(string source, string message, string memberName = "", int lineNumber = 0, string filePath = "");
-        void LogWarning(string source, string message, string memberName = "", int lineNumber = 0,string filePath = "");
+        void LogEvent(string source, string message, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "");
+        void LogWarning(string source, string message, string memberName = "", int lineNumber = 0, string filePath = "");
 
-        void LogDebug(string source, string message, string memberName = "", int lineNumber = 0, string filePath = "");
+        void LogDebug(string source, string message, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "");
 
-        void LogError(string source, string message, string memberName = "", int lineNumber = 0, string filePath = "");
+        void LogError(string source, string message, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "");
 
-        void LogCritical(string source, string message, string memberName = "", int lineNumber = 0,string filePath = "");
+        void LogCritical(string source, string message, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "");
 
-        void LogException(Exception ex, string source, string message, string memberName = "", int lineNumber = 0,string filePath = "");
+        void LogException(Exception ex, string source, string message, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "");
     }
 }

1553f16671fb595f3c26ef9ccca985b695c3f507
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-21 21:03:12 +0300

    update analogy extension

---
+++ Analogy.Interfaces/Factories/IAnalogyExtensionsFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analogy.Interfaces.Factories
+{
+    interface IAnalogyExtensionsFactory
+    {        /// <summary>
+        /// the factory id which this Extensions factory belongs to
+        /// </summary>
+        Guid FactoryId { get; }
+        string Title { get; }
+        IEnumerable<IAnalogyExtension> DataProviders { get; }
+    }
+}
--- Analogy.Interfaces/IAnalogyExtension.cs
+++ Analogy.Interfaces/IAnalogyExtension.cs
@@ -5,19 +5,18 @@ namespace Analogy.Interfaces
 {
     public interface IAnalogyExtension
     {
-        Guid ExtensionID { get; }
+        Guid ID { get; }
         string Author { get; }
         string AuthorMail { get; }
         List<string> AdditionalContributors { get; }
-        string DisplayName { get; }
+        string Title { get; }
         string Description { get; }
-        AnalogyExtensionType AnalogyExtensionType { get; }
+        AnalogyExtensionType ExtensionType { get; }
 
         void CellClicked(object sender, AnalogyCellClickedEventArgs args);
         object GetValueForCellColumn(AnalogyLogMessage message, string columnName);
         List<AnalogyColumnInfo> GetColumnsInfo();
         void NewMessage(AnalogyLogMessage message);
         void NewMessages(List<AnalogyLogMessage> messages);
-        void LaunchUIElement();
     }
 }

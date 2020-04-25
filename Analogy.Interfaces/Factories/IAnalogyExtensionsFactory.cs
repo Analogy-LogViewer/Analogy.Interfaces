905129ccd68c826e71352d29da48b2bac2de9bd0
Lior Banai <liorbanai@gmail.com>
2020-04-25 20:56:41 +0300

    update extension interface

--- Analogy.Interfaces/Factories/IAnalogyExtensionsFactory.cs
+++ Analogy.Interfaces/Factories/IAnalogyExtensionsFactory.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Analogy.Interfaces.Factories
 {
     interface IAnalogyExtensionsFactory
     {        /// <summary>
-        /// the factory id which this Extensions factory belongs to
-        /// </summary>
+             /// the factory id which this Extensions factory belongs to
+             /// </summary>
         Guid FactoryId { get; }
         string Title { get; }
-        IEnumerable<IAnalogyExtension> DataProviders { get; }
+        IEnumerable<IAnalogyExtension> Extensions { get; }
     }
 }

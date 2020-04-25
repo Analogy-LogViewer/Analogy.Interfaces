94fc1f9db9ffb050656a6670c9db6aee4c3f7484
Lior Banai <liorbanai@gmail.com>
2020-04-25 21:31:28 +0300

    fix visiblity

--- Analogy.Interfaces/Factories/IAnalogyExtensionsFactory.cs
+++ Analogy.Interfaces/Factories/IAnalogyExtensionsFactory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Analogy.Interfaces.Factories
 {
-    interface IAnalogyExtensionsFactory
+   public interface IAnalogyExtensionsFactory
     {
         /// <summary>
         /// the factory id which this Extensions factory belongs to

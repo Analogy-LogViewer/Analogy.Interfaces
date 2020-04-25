df080514a19734d0ca198da83a86c3ccace41428
Lior Banai <liorbanai@gmail.com>
2020-04-25 21:03:43 +0300

    ..

--- Analogy.Interfaces/Factories/IAnalogyExtensionsFactory.cs
+++ Analogy.Interfaces/Factories/IAnalogyExtensionsFactory.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 namespace Analogy.Interfaces.Factories
 {
     interface IAnalogyExtensionsFactory
-    {        /// <summary>
-             /// the factory id which this Extensions factory belongs to
-             /// </summary>
+    {
+        /// <summary>
+        /// the factory id which this Extensions factory belongs to
+        /// </summary>
         Guid FactoryId { get; }
         string Title { get; }
         IEnumerable<IAnalogyExtension> Extensions { get; }

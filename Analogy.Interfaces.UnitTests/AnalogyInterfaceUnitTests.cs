83f76619465ef02f1094064964af4771bdd14af0
Lior Banai <liorbanai@gmail.com>
2020-06-17 17:10:00 +0300

    Add Unit Test project

---
+++ Analogy.Interfaces.UnitTests/AnalogyInterfaceUnitTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Analogy.Interfaces.UnitTests
+{
+    [TestClass]
+    public class AnalogyInterfaceUnitTests
+    {
+        [TestMethod]
+        public void TestAnalogyLogMessageIsNotNull()
+        {
+            AnalogyLogMessage message = new AnalogyLogMessage();
+            Assert.IsNotNull(message.Text);
+            Assert.IsNotNull(message.Category);
+            Assert.IsNotNull(message.Source);
+            Assert.IsNotNull(message.FileName);
+            Assert.IsNotNull(message.MethodName);
+            Assert.IsNotNull(message.Module);
+            Assert.IsNotNull(message.User);
+            Assert.IsNotNull(message.Parameters);
+        }
+    }
+}

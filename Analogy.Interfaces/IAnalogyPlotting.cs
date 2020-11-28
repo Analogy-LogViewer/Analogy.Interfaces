ea2868249596e7c539ea3e06d90015ac2f54bb96
Lior Banai <liorbanai@gmail.com>
2020-11-28 21:17:59 +0200

    add factoryId

--- Analogy.Interfaces/IAnalogyPlotting.cs
+++ Analogy.Interfaces/IAnalogyPlotting.cs
@@ -10,6 +10,10 @@ namespace Analogy.Interfaces
         event EventHandler<AnalogyPlottingPointData> OnNewPointData;
         IEnumerable<(string SeriesName, AnalogyPlottingSeriesType SeriesViewType)> GetChartSeries();
         Guid Id { get; set; }
+        /// <summary>
+        /// the factory id which this Data providers factory belongs to
+        /// </summary>
+        Guid FactoryId { get; set; }
         string Title { get; set; }
         Task InitializePlottingAsync(IAnalogyLogger logger);
         Task StartPlotting();

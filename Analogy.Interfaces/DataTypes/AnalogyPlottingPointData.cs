8ba5a689e71641e57734a93c6bc94dbca94ea344
Lior Banai <liorbanai@gmail.com>
2020-11-28 19:45:25 +0200

    update

--- Analogy.Interfaces/DataTypes/AnalogyPlottingPointData.cs
+++ Analogy.Interfaces/DataTypes/AnalogyPlottingPointData.cs
@@ -1,15 +1,19 @@
-﻿namespace Analogy.Interfaces.DataTypes
+﻿using System;
+
+namespace Analogy.Interfaces.DataTypes
 {
     public class AnalogyPlottingPointData
     {
 
         public string Name { get; set; }
         public double Value { get; set; }
+        public DateTime DateTime { get; }
 
-        public AnalogyPlottingPointData(string name, double value)
+        public AnalogyPlottingPointData(string name, double value, DateTime dateTime)
         {
             Name = name;
             Value = value;
+            DateTime = dateTime;
         }
     }
 }

cbaff7347d814a644d86e9224baae1aa7d98e84b
Lior Banai <liorbanai@gmail.com>
2020-12-06 18:26:06 +0200

    fix #38, fix #40, Fix #41, fix #42, fix #43

---
+++ Analogy.Interfaces/DataTypes/AnalogyToolTip.cs
@@ -0,0 +1,16 @@
+﻿namespace Analogy.Interfaces.DataTypes
+{
+    public class AnalogyToolTip
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Footer { get; set; }
+
+        public AnalogyToolTip(string title, string content, string footer)
+        {
+            Title = title;
+            Content = content;
+            Footer = footer;
+        }
+    }
+}

658e6514e709045dbbb28c91fd53d4256dcda15a
Lior Banai <liorbanai@gmail.com>
2021-01-01 14:32:01 +0200

    add optional images to tooltip

--- Analogy.Interfaces/DataTypes/AnalogyToolTip.cs
+++ Analogy.Interfaces/DataTypes/AnalogyToolTip.cs
@@ -1,16 +1,27 @@
-﻿namespace Analogy.Interfaces.DataTypes
+﻿using System.Drawing;
+
+namespace Analogy.Interfaces.DataTypes
 {
     public class AnalogyToolTip
     {
         public string Title { get; set; }
         public string Content { get; set; }
         public string Footer { get; set; }
-
-        public AnalogyToolTip(string title, string content, string footer)
+        /// <summary>
+        /// 16x16 Image
+        /// </summary>
+        public Image? SmallImage { get; set; }
+        /// <summary>
+        /// 32x32 Image
+        /// </summary>
+        public Image? LargeImage { get; set; }
+        public AnalogyToolTip(string title, string content, string footer, Image? smallImage, Image? largeImage)
         {
             Title = title;
             Content = content;
             Footer = footer;
+            SmallImage = smallImage;
+            LargeImage = largeImage;
         }
     }
 }

3c479a503db37a36f67d18449efc86ffb8bb0907
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2021-03-26 10:17:03 +0300

    add missing interface for user controls

---
+++ Analogy.Interfaces/Factories/IAnalogyCustomUserControlsFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Analogy.Interfaces.Factories
+{
+    public interface IAnalogyCustomUserControlsFactory
+    {        /// <summary>
+             /// the factory id which this actions providers factory belongs to
+             /// </summary>
+        Guid FactoryId { get; set; }
+        string Title { get; set; }
+        IEnumerable<IAnalogyCustomUserControl> Actions { get; }
+    }
+}

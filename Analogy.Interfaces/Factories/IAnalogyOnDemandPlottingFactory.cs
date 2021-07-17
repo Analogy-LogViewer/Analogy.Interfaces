7b720a32655c8621888dff43b5359e828952d0f6
Lior Banai <liorbanai@gmail.com>
2021-07-17 19:28:23 +0300

    ..

---
+++ Analogy.Interfaces/Factories/IAnalogyOnDemandPlottingFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Analogy.Interfaces.Factories
+{
+    public interface IAnalogyOnDemandPlottingFactory
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<IAnalogyOnDemandPlotting> OnDemandPlottingGenerator { get; set; }
+        public event EventHandler<IAnalogyOnDemandPlotting> OnAddedOnDemandPlottingGenerator;
+        public event EventHandler<IAnalogyOnDemandPlotting> OnRemovedOnDemandPlottingGenerator;
+    }
+}

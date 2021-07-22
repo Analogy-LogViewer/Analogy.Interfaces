53f7ce4d8cc6d60be95164b2e8c6448506abbef3
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2021-07-22 17:18:31 +0300

    ..

--- Analogy.Interfaces/IAnalogyOnDemandPlottingInteractor.cs
+++ Analogy.Interfaces/IAnalogyOnDemandPlottingInteractor.cs
@@ -6,6 +6,7 @@ namespace Analogy.Interfaces
     {
         public void ShowPlot(Guid id,string plotTitle, AnalogyOnDemandPlottingStartupType startupType);
         public void ClosePlot(Guid id);
+        public void AddSeriesToPlot(Guid id, string seriesName);
         public void RemoveSeriesFromPlot(Guid id, string seriesName);
         public void ClearSeriesData(Guid id, string seriesNameToClear);
         public void ClearAllData(Guid id);

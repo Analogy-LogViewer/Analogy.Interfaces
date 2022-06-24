b036f3005daad0c671695e9bac3ae19c63bc2ada
Lior Banai Kama <liorbanai@gmail.com>
2022-06-24 13:50:15 +0300

    API change

--- Analogy.Interfaces/DataTypes/FilterCriteria.cs
+++ Analogy.Interfaces/DataTypes/FilterCriteria.cs
@@ -8,31 +8,33 @@ namespace Analogy.Interfaces.DataTypes
 {
     public class FilterCriteria
     {
-        public List<string> TextInclude { get; set; }
-        public List<string> TextExclude { get; set; }
-        public List<string> Sources { get; set; }
-        public List<string> ExcludedSources { get; set; }
-        public List<string> Modules { get; set; }
-        public List<string> ExcludedModules { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public AnalogyLogLevel[] IncludeLevels { get; set; }
-        public List<AnalogyColumnFilter> DynamicColumns { get; set; }
+        public IEnumerable<string> IncludeText { get; set; }
+        public IEnumerable<string> ExcludeText { get; set; }
+        public IEnumerable<string> IncludeSources { get; set; }
+        public IEnumerable<string> ExcludeSources { get; set; }
+        public IEnumerable<string> IncludeModules { get; set; }
+        public IEnumerable<string> ExcludeModules { get; set; }
+        public IEnumerable<AnalogyLogLevel> IncludeLevels { get; set; }
+        public IEnumerable<AnalogyLogLevel> ExcludeLevels { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public IEnumerable<AnalogyColumnFilter> DynamicColumns { get; set; }
 
-        public FilterCriteria(List<string> textInclude, List<string> textExclude, List<string> sources,
-            List<string> excludedSources, List<string> modules, List<string> excludedModules, DateTime startTime,
-            DateTime endTime, AnalogyLogLevel[] includeLevels, List<AnalogyColumnFilter> dynamicColumns)
+        public FilterCriteria(List<string>? includeText = null, List<string>? excludeText = null, List<string>? includeSources = null,
+            List<string>? excludeSources = null, List<string>? includeModules = null, List<string>? excludeModules = null, DateTime? startTime = null,
+            DateTime? endTime = null, List<AnalogyLogLevel>? includeLevels = null, List<AnalogyLogLevel>? excludeLevels = null, List<AnalogyColumnFilter>? dynamicColumns = null)
         {
-            TextInclude = textInclude;
-            TextExclude = textExclude;
-            Sources = sources;
-            ExcludedSources = excludedSources;
-            Modules = modules;
-            ExcludedModules = excludedModules;
+            IncludeText = includeText ?? Enumerable.Empty<string>();
+            ExcludeText = excludeText ?? Enumerable.Empty<string>();
+            IncludeSources = includeSources ?? Enumerable.Empty<string>();
+            ExcludeSources = excludeSources ?? Enumerable.Empty<string>();
+            IncludeModules = includeModules ?? Enumerable.Empty<string>();
+            ExcludeModules = excludeModules ?? Enumerable.Empty<string>();
             StartTime = startTime;
             EndTime = endTime;
-            IncludeLevels = includeLevels;
-            DynamicColumns = dynamicColumns;
+            IncludeLevels = includeLevels ?? Enumerable.Empty<AnalogyLogLevel>();
+            ExcludeLevels = excludeLevels ?? Enumerable.Empty<AnalogyLogLevel>();
+            DynamicColumns = dynamicColumns ?? Enumerable.Empty<AnalogyColumnFilter>();
         }
 
 
---
+++ Analogy.Interfaces/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analogy.Interfaces
+{
+    public static class Utils
+    {
+        public static IEnumerable<AnalogyLogLevel> AllLogLevels { get; } = Enum.GetValues(typeof(AnalogyLogLevel)).Cast<AnalogyLogLevel>();
+    }
+}

c8c20bdf2d914c9bb194384868aa294a7e2dac58
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2022-09-14 19:48:02 +0300

    Add AnalogyMessageInteractor Middleware To Enable modification of message after parsing

---
+++ Analogy.Interfaces/IAnalogyMessageInteractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analogy.Interfaces
+{
+
+    /// <summary>
+    /// interface to allow message modification before presenting in the UI 
+    /// </summary>
+    internal interface IAnalogyLogMessageInteractor
+    {
+        Task<IAnalogyLogMessage> HandleMessage(IAnalogyLogMessage message);
+    }
+}
